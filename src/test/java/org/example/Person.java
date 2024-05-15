aa13ffd4997a4a47b1d8aafa35d930321a4db992
Маркевич Сергей Андреевич <SMarkevich@synergy.ru>
2024-05-15 16:12:23 +0300

    dorabotka

--- src/test/java/org/example/Person.java
+++ src/test/java/org/example/Person.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.junit.Test;
 
 public class Person {
@@ -17,14 +18,20 @@ public class Person {
     }
 
     public static PersonBuilder builder() {
-        return  new PersonBuilder();
+        return  new PersonBuilder("",  "", 0, 0);
     }
 
-    public static class PersonBuilder {
+    public static class PersonBuilder extends Person{
+
+        public PersonBuilder(String name, String secondName, int age, int classLen) {
+            super(name, secondName, age, classLen);
+        }
+        /*
         public String name;
         public String secondName;
         public int age;
         public int classLen;
+        */
 
         public PersonBuilder name(String name) {
             this.name = name;
